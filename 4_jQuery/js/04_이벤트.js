a65b38f7d43a86564c00c7c2bcce7cdff4a92816
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-07-28 15:34:26 +0900

    feat: 2023.07.28

--- 4_jQuery/js/04_이벤트.js
+++ 4_jQuery/js/04_이벤트.js
@@ -24,68 +24,121 @@ $('#area1').on('click', function(event) {
     // 배경색은 white, 텍스트는 ''
     $(event.target).css('background-color', 'white').text('').off('mouseenter mouseleave');
 });*/
-$('#area1').on({
-    mouseenter:function(event){
-        $(event.target).css('background-color', 'beige').text('마우스가 올라감');
-    },
-    mouseleave:function(event){
-        $(event.target).css('background-color', 'hotpink').text('마우스가 내려감');
-    },
-    click:function(event) {
-        $(event.target).css('background-color', 'white').text('').off('mouseenter mouseleave');
-    }
+$("#area1").on({
+  mouseenter: function (event) {
+    $(event.target).css("background-color", "beige").text("마우스가 올라감");
+  },
+  mouseleave: function (event) {
+    $(event.target).css("background-color", "hotpink").text("마우스가 내려감");
+  },
+  click: function (event) {
+    $(event.target)
+      .css("background-color", "white")
+      .text("")
+      .off("mouseenter mouseleave");
+  },
 });
 
 // 2) one
-$('#area2').one('click', function() {
-    alert('실행!');
+$("#area2").one("click", function () {
+  alert("실행!");
 });
 
 // 2. 키보드 이벤트
 // 1) keydown, keypress, keyup
 // keydown : 키보드가 눌려질 때
-$('#textarea1').keydown(function(e) {
-    console.log(`keydown / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+$("#textarea1").keydown(function (e) {
+  console.log(`keydown / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
 });
 // keypress : 글자가 입력될 때
-$('#textarea1').keypress(function(e) {
-    console.log(`keypress / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+$("#textarea1").keypress(function (e) {
+  console.log(`keypress / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
 });
 // keyup : 키보드가 떼어질 때
-$('#textarea1').keyup(function(e) {
-    console.log(`keyup / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+$("#textarea1").keyup(function (e) {
+  console.log(`keyup / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
 });
 
 // -> 3가지를 on 메소드로 한번에!
-$('#textarea1').on({
-    keydown: function(e) {
-        console.log(`keydown / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
-    },
-    keypress: function(e) {
-        console.log(`keypress / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
-    },
-    keyup: function(e) {
-        console.log(`keyup / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
-    }
+$("#textarea1").on({
+  keydown: function (e) {
+    console.log(`keydown / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+  },
+  keypress: function (e) {
+    console.log(`keypress / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+  },
+  keyup: function (e) {
+    console.log(`keyup / e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+  },
 });
 
 // substring
 console.log("Hello~".substring(0, 2));
 
-$('#textarea2').on('keyup', function(e) {
-    const currentLength = $(e.target).val().length;
-    const maxLength = parseInt($('#maxLength').text());
-    // console.log(typeof maxLength);
+$("#textarea2").on("keyup", function (e) {
+  const currentLength = $(e.target).val().length;
+  const maxLength = parseInt($("#maxLength").text());
+  // console.log(typeof maxLength);
 
-    
+  if (currentLength > maxLength) {
+    // 글자수제한! substring!
+    // $(e.target).val().substring(0, maxLength); // 50글자
+    $(e.target).val($(e.target).val().substring(0, maxLength));
+  } else {
+    // currentLength <= maxLength
+    $("#counter").text(currentLength);
+  }
+});
 
-    if(currentLength > maxLength) {
-        // 글자수제한! substring!
-        // $(e.target).val().substring(0, maxLength); // 50글자
-        $(e.target).val($(e.target).val().substring(0, maxLength));
-    } else { // currentLength <= maxLength
-        $('#counter').text(currentLength);
-    }
+// 3) 아이디 조건 확인
+$("#userId").keyup(function (e) {
+  const regExp = /^[a-z][a-z0-9]{4,12}$/;
+  const id = $(e.target).val();
+  if (regExp.test(id)) {
+    // 사용 가능한 아이디입니다.
+    $("#idCheck").text("사용 가능한 아이디입니다.").css("color", "green");
+  } else if (id === "") {
+    $("#idCheck").text("");
+  } else {
+    // 사용 불가능한 아이디입니다.
+    $("#idCheck").text("사용 불가능한 아이디입니다.").css("color", "red");
+  }
+});
+// 사용 가능한 아이디 입니다.
+// 사용 불가능한 아이디 입니다.
+// const userId = document.querySelector('#userId');
+// function userIdCheck() {
+//     const regExp = /^[a-z][a-z0-9]{4,12}$/;
+//     return regExp.test(userId.value);
+// }
+// function validate() {
+//     if(!userIdCheck()) {
+//         userId.focus();
+//         return false;
+//     } else {
+//         return true;
+//     }};
+// function inputHandler(event, check, message) {
+//     if(!check) {
+//         event.target.nextElementSibling.style.color = 'red';
+//         event.target.nextElementSibling.innerHTML = message;
+//     } else {
+//         event.target.nextElementSibling.style.color = 'green';
+//         event.target.nextElementSibling.innerHTML = '사용 가능한 아이디 입니다.';
+//     }
+// }
+// userId.addEventListener('input', function() {
+//     inputHandler(event, userIdCheck(), '사용 불가능한 아이디 입니다.');
+// }); --- 내가 한거
 
+// 3. trigger() 메소드
+// let num = 0; --대부분이 한 방식
+$("#area3").on("click", function () {
+  // $('#counter2').text(++num); --대부분이 한 방식
 
+  let currentCount = parseInt($("#counter2").text());
+  $("#counter2").text(++currentCount);
+});
+$("#btn").on("click", function () {
+  $("#area3").trigger("click");
 });
